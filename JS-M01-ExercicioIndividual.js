180769e9a1f077d3f56e89125668f8c1507f104f
fennec001 <109749936+fennec001@users.noreply.github.com>
2023-02-22 07:19:54 -0300

    Exercicio 1 inserido dentro do html

--- JS-M01-ExercicioIndividual.js
+++ JS-M01-ExercicioIndividual.js
@@ -1,10 +1,11 @@
 //Escreva um programa e declare 5 variáveis de tipos diferentes. imprima no console o
 // valor e o tipo de cada variável declarada.
 
-// declaração de variáveis
+function arrayDeTipos(){// declaração de variáveis
 const arr = [NaN,"string",null,false,BigInt(0)]
 
 // Imprimindo o tipo de cada variavel
 for(x in arr){
  console.log(typeof arr[x])
+}
 }
