80c9603b3b6772f13b07833154a820f9fdb18669
fennec001 <109749936+fennec001@users.noreply.github.com>
2023-02-21 23:42:10 -0300

    Original files

---
+++ JS-M02-ExercicioIndividual.js
@@ -0,0 +1,10 @@
+// funcao para retornar o salario de acordo com o calculo
+function salarioMensal(){
+    let dias = document.getElementById("dias").value;
+    let salarioDiario = (document.getElementById("salario").value / 22);
+    return (salarioDiario*dias);
+}
+
+function escreverSalario(){
+pegar o individual da ariane para dar uma olhadinha
+}
